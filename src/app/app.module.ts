605a14a41435eb5732490c21168dc8ecd5500ae2
Artem <aprricod@gmail.com>
2021-02-28 17:49:29 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+import { Comp4Component } from './comp4/comp4.component';
+import { Comp5Component } from './comp5/comp5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    Comp2Component,
+    Comp3Component,
+    Comp4Component,
+    Comp5Component,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [Comp3Component, Comp4Component, Comp5Component],
 })
-export class AppModule { }
+export class AppModule {}
